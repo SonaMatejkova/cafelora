fd23cd1182e206ae7f1f33b5bfd3448e6da00712
Soňa Matějková <mat.sona53@gmail.com>
2020-11-01 12:20:13 +0100

    Rozbalování a skrývání navigace

--- src/index.js
+++ src/index.js
@@ -2,3 +2,17 @@ import './index.html';
 import './style.css';
 
 console.log('funguju!');
+
+const navBtn = document.querySelector('#nav-btn');
+navBtn.addEventListener('click', () => {
+  const nav = document.querySelector('nav');
+  nav.classList.toggle('nav-closed');
+});
+
+const odkaz = document.querySelectorAll('a');
+for (let i = 0; i < odkaz.length; i++) {
+  odkaz[i].addEventListener('click', () => {
+    const nav = document.querySelector('nav');
+    nav.classList.toggle('nav-closed');
+  });
+}
