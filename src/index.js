a707fc375157ab7180288c5b8eca37e8d10253bf
Soňa Matějková <mat.sona53@gmail.com>
2020-11-01 16:56:54 +0100

    Nefunguje komponenta Drink

--- src/index.js
+++ src/index.js
@@ -18,21 +18,6 @@ for (let i = 0; i < odkaz.length; i++) {
   });
 }
 
-let ordered = false;
-const orderBtn = document.querySelector('.order-btn');
-orderBtn.addEventListener('click', () => {
-  document
-    .querySelector('.drink__cup')
-    .classList.toggle('drink__cup--selected');
-  if (ordered === false) {
-    orderBtn.textContent = 'Zrušit';
-    ordered = true;
-  } else {
-    orderBtn.textContent = 'Objednat';
-    ordered = false;
-  }
-});
-
 const LayerList = (props) => {
   let layerList = '';
   for (let i = 0; i < props.items.length; i++) {
@@ -40,7 +25,7 @@ const LayerList = (props) => {
   }
   return layerList;
 };
-
+/*
 const drinkInfo = document.querySelector('.drink__info');
 const show = (list) => {
   drinkInfo.innerHTML = `<h3>Cappuccino</h3> ${LayerList({ items: list })}`;
@@ -61,4 +46,52 @@ const layers = [
   },
 ];
 
-show(layers);
+show(layers);*/
+
+const drinks = {
+  id: 'romano',
+  name: 'Romano',
+  ordered: false,
+  layers: [
+    {
+      color: '#fbdf5b',
+      label: 'citrón',
+    },
+    {
+      color: '#613916',
+      label: 'espresso',
+    },
+  ],
+};
+const Drink = (props) => {
+  const drink = document.createElement('div');
+  drink.className = 'drink';
+  drink.innerHTML = `
+            <div class="drink__product">
+              <div class="drink__cup">
+                <img src="/assets/cups/${props.id}.png" />
+              </div>
+              <div class="drink__info"><h3>${props.name}</h3>${LayerList({
+    items: props.layers,
+  })}</div>
+            </div>
+            <div class="drink__controls">
+              <button class="order-btn">Objednat</button>
+            </div>`;
+  const orderBtn = drink.querySelector('.order-btn');
+  orderBtn.addEventListener('click', () => {
+    let ordered = props.ordered;
+    document
+      .querySelector('.drink__cup')
+      .classList.toggle('drink__cup--selected');
+    if (ordered === false) {
+      orderBtn.textContent = 'Zrušit';
+      ordered = true;
+    } else {
+      orderBtn.textContent = 'Objednat';
+      ordered = false;
+    }
+  });
+};
+const drinksList = document.querySelector('.drinks-list');
+drinksList.appendChild = Drink(drinks);
