25b8bf99b4ea106990f0d55c8f0f1ab5e66da9b6
Soňa Matějková <mat.sona53@gmail.com>
2020-11-01 13:22:47 +0100

    Objednávací tlačítko funguje

--- src/index.js
+++ src/index.js
@@ -13,6 +13,21 @@ const odkaz = document.querySelectorAll('a');
 for (let i = 0; i < odkaz.length; i++) {
   odkaz[i].addEventListener('click', () => {
     const nav = document.querySelector('nav');
-    nav.classList.toggle('nav-closed');
+    nav.classList.add('nav-closed');
   });
 }
+
+let ordered = false;
+const orderBtn = document.querySelector('.order-btn');
+orderBtn.addEventListener('click', () => {
+  document
+    .querySelector('.drink__cup')
+    .classList.toggle('drink__cup--selected');
+  if (ordered === false) {
+    orderBtn.textContent = 'Zrušit';
+    ordered = true;
+  } else {
+    orderBtn.textContent = 'Objednat';
+    ordered = false;
+  }
+});
